aa66381e290a39e6d9122893150490aae861ed85
Mayan Sequeira <mayan.sequeira@gmail.com>
2025-07-15 09:23:44 +0530

    streamlit deployment time

--- stream/app.py
+++ stream/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-fastapi_url = "http://localhost:8000/" # CHANGE DURING DEPLOYMENT
+fastapi_url = "https://audiocompass.onrender.com/" # THE FASTAPI SERVER IS LIVE
 
 st.title("🎵 Audio Compass")
 
